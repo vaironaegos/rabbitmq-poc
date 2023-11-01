cab04bdf820e33f21ee7c20a2f72fcbbcb5d171a
Kilderson Sena <dersonsena@gmail.com>
2023-11-01 17:49:43 -0300

    first project commit

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+require __DIR__ . '/../vendor/autoload.php';
+
+date_default_timezone_set('America/Sao_Paulo');
+locale_set_default('pt_br');
+
+echo 'Hello World';
