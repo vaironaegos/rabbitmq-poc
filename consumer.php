849c5279e80806f1470508d4c17645086c4c51b1
Kilderson Sena <dersonsena@gmail.com>
2023-11-01 22:21:37 -0300

    chore: finish video record

--- consumer.php
+++ consumer.php
@@ -1,5 +1,28 @@
 <?php
 
-while (true) {
-    // Do anything
+declare(strict_types=1);
+
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+require __DIR__ . '/vendor/autoload.php';
+
+date_default_timezone_set('America/Sao_Paulo');
+
+[, $queueName] = explode('=', $argv[2]);
+
+$connection = new AMQPStreamConnection('mail-service-rabbitmq', '5672', 'guest', 'guest');
+$channel = $connection->channel();
+
+$channel->exchange_declare('my-exchange', AMQP_EX_TYPE_DIRECT, false, true, false);
+$channel->queue_declare($queueName, false, true, false, false);
+$channel->queue_bind($queueName, 'my-exchange', $queueName);
+
+$channel->basic_consume($queueName, '', false, false, false, false, function(AMQPMessage $message) {
+    echo $message->body . PHP_EOL;
+    $message->getChannel()->basic_ack($message->getDeliveryTag());
+});
+
+while ($channel->is_open()) {
+    $channel->wait();
 }
--- public/index.php
+++ public/index.php
@@ -2,9 +2,48 @@
 
 declare(strict_types=1);
 
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
 require __DIR__ . '/../vendor/autoload.php';
 
 date_default_timezone_set('America/Sao_Paulo');
-locale_set_default('pt_br');
 
-echo 'Hello World';
+$connection = new AMQPStreamConnection('mail-service-rabbitmq', '5672', 'guest', 'guest');
+$channel = $connection->channel();
+
+$channel->exchange_declare('my-exchange', AMQP_EX_TYPE_DIRECT, false, true, false);
+$channel->queue_declare('mailer', false, true, false, false);
+$channel->queue_declare('xpto', false, true, false, false);
+$channel->queue_declare('abc', false, true, false, false);
+
+$channel->queue_bind('mailer', 'my-exchange', 'mailer');
+$channel->queue_bind('xpto', 'my-exchange', 'xpto');
+$channel->queue_bind('abc', 'my-exchange', 'abc');
+
+$message1 = json_encode([
+    'action' => 'send-mail',
+    'id' => uniqid(),
+    'name' => 'Pietro Coelho - mailer',
+    'email' => 'pietro@astrotech.solutions',
+]);
+
+$message2 = json_encode([
+    'action' => 'send-mail',
+    'id' => uniqid(),
+    'name' => 'Pietro Coelho - XPTO',
+    'email' => 'pietro@astrotech.solutions',
+]);
+
+$message3 = json_encode([
+    'action' => 'send-mail',
+    'id' => uniqid(),
+    'name' => 'Pietro Coelho - ABC',
+    'email' => 'pietro@astrotech.solutions',
+]);
+
+$channel->basic_publish(msg: new AMQPMessage($message1), exchange: 'my-exchange', routing_key: 'mailer');
+$channel->basic_publish(msg: new AMQPMessage($message2), exchange: 'my-exchange', routing_key: 'xpto');
+$channel->basic_publish(msg: new AMQPMessage($message3), exchange: 'my-exchange', routing_key: 'abc');
+
+echo 'Conectado no RabbitMQ!';
